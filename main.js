4e4d090fc8d9a19efe87d4f2ac21da63f01574ea
Newish0 <80089521+Newish0@users.noreply.github.com>
2022-03-12 21:43:54 -0800

    Update to Jikan4 and demo page

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 
 
 
-window.addEventListener("DOMContentLoaded", () => {
-
+$(() => {
 
 
     let mal = new Jikan4();
@@ -16,18 +15,24 @@ window.addEventListener("DOMContentLoaded", () => {
             display(galleryPosterCon);
             setTimeout(armLoadMoreBtn, 300);
         });
-        
 
+
+
+    // init filters
     let filters = document.querySelector("#filters");
+    let scoreRange = { min: 0, max: 10 };
 
-    filters.addEventListener("change", () => {
+    filters.addEventListener("change", () => filterSearch());
+
+    function filterSearch() {
         let params = {};
         let tags = "";
+        let producers = "";
 
         let search = document.querySelector("#filter-search-box").value;
 
         params.q = search;
-        
+
 
         document.querySelectorAll("#filter-tags input").forEach((n) => {
             if (n.checked) {
@@ -36,11 +41,38 @@ window.addEventListener("DOMContentLoaded", () => {
         });
         tags = tags.substring(0, tags.length - 1);
         params.genres = tags;
+        
 
+        params.minScore = scoreRange.min;
+        params.maxScore = scoreRange.max;
+
+
+        document.querySelectorAll("#filter-studios input").forEach((n) => {
+            if (n.checked) {
+                producers += n.value + ",";
+            }
+        });
+        producers = producers.substring(0, producers.length - 1);
+        params.producers = producers;
 
 
         display(galleryPosterCon, "anime_search", params);
+    }
+
+    $("#score-range-slider").slider({
+        range: true,
+        min: 0,
+        max: 10,
+        values: [0, 10],
+        slide: function (event, ui) {
+            $("#score-range-out").val(ui.values[0] + " - " + ui.values[1]);
+            scoreRange.min = ui.values[0];
+            scoreRange.max = ui.values[1];
+            filterSearch();
+        }
     });
+    $("#score-range-out").val($("#score-range-slider").slider("values", 0) +
+        " - " + $("#score-range-slider").slider("values", 1));
 
     mal.getAnimeGenres({
         filter: "genres"
@@ -60,6 +92,24 @@ window.addEventListener("DOMContentLoaded", () => {
         }
 
         document.querySelector("#filter-tags").innerHTML += htmlTxt;
+    });
+
+    mal.getProducers().then(producers => {
+        producers = producers.data;
+        let htmlTxt = "";
+        for (let x in producers) {
+
+            let name = producers[x].name;
+            let value = producers[x].mal_id;
+            htmlTxt += htmlTxt = `
+            <span>
+                <input type="checkbox" name="${name}" class="filter-form-inc-exc-tags" id="${name}-${value}" value="${value}">
+                <label class="unselectable" for="${name}-${value}">${name}</label>
+            </span>
+            `;
+        }
+
+        document.querySelector("#filter-studios").innerHTML += htmlTxt;
     });
 
 
@@ -84,9 +134,12 @@ window.addEventListener("DOMContentLoaded", () => {
                 //   entry.rootBounds
                 //   entry.target
                 //   entry.time
-                console.log(entry.target)
+                if(galleryPosterCon.childNodes.length > 1) {
+                    console.log("[DEBUG] Auto load more " + entry.target);
+                    displayMore(mal, galleryPosterCon);
+                }
 
-                displayMore(mal, galleryPosterCon);
+                
             });
         };
 
@@ -112,14 +165,14 @@ window.addEventListener("DOMContentLoaded", () => {
             case "anime_top":
             default:
                 displayTopAnime(mal, container);
-                //setInterval(() => {displayMore(mal, galleryPosterCon)}, 2000)
+            //setInterval(() => {displayMore(mal, galleryPosterCon)}, 2000)
         }
     }
 
     function displayMore(api, container) {
         api.getMore().then((res) => {
 
-            if(res != null) {
+            if (res != null) {
                 let data = res.data;
                 console.log(data)
                 for (let i in data) {
@@ -127,6 +180,8 @@ window.addEventListener("DOMContentLoaded", () => {
                     container.appendChild(card);
                 }
             }
+        }).catch(function () {
+            console.log("TOO MANY REQUEST");
         });
     }
 
@@ -140,6 +195,8 @@ window.addEventListener("DOMContentLoaded", () => {
                 let card = createCard(cardTemplate, data[i]);
                 container.appendChild(card);
             }
+        }).catch(function () {
+            console.log("TOO MANY REQUEST");
         });
     }
 
@@ -151,6 +208,8 @@ window.addEventListener("DOMContentLoaded", () => {
                 let card = createCard(cardTemplate, data[i]);
                 container.appendChild(card);
             }
+        }).catch(function () {
+            console.log("TOO MANY REQUEST");
         });
     }
 
@@ -240,4 +299,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
         return tmpCon.querySelector(".poster-card");
     }
+
+    // https://stackoverflow.com/questions/54246477/how-to-convert-camelcase-to-snake-case-in-javascript
+    function camelToSnakeCase(str) { str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`); }
 });
